4f02033b5ea2e8e18170721e0e9a058dc9c4ec90
chsi12 <144258406+chsi12@users.noreply.github.com>
2025-03-03 13:58:07 -0500

    mod6

---
+++ mod6/js/app.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+angular.module('LunchChecker',[])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+    
+function LunchCheckController ($scope) {
+   $scope.checkResult = "";
+   $scope.checkLunch = function () {
+    var res = checkLunch($scope.itemlist);
+    $scope.msgStyle = res[0];
+    $scope.textBoxStyle = res[0];
+    $scope.checkResult = res[1];
+  };
+
+   function checkLunch(string) {
+    var msg = "Please enter data first";
+    if(!string) {
+        return ["red",msg];
+    }
+    //removes filters out empty entries
+    var items = string.split(",").filter(isEmpty);
+    
+    if(items.length > 3) {
+        msg = "Too much!";
+    } else if(items.length > 0) {
+        msg = "Enjoy!";
+    }
+    return ["green",msg];
+  }
+
+  function isEmpty (string) {
+    return string.trim() !== '';
+  }
+}
+})();
